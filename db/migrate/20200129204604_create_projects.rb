e7eedfe1705d18093fe98e8b768032a3e42eba19
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-01-29 15:56:42 -0500

    migrations

--- db/migrate/20200129204604_create_projects.rb
+++ db/migrate/20200129204604_create_projects.rb
@@ -1,7 +1,11 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.string :module
+      t.string :description
+      t.string :github
+      t.integer :user_id
       t.timestamps null: false
     end
   end
