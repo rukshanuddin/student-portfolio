93fbabd1cdfc1254a12b90a76d755f1d491f9cf8
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-01-29 17:05:51 -0500

    projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,37 +1,127 @@
 class ProjectsController < ApplicationController
-
-  # GET: /projects
-  get "/projects" do
-    erb :"/projects/index.html"
+  get '/projects' do
+    if logged_in?
+      @projects = Project.all
+      erb :'projects/index.html'
+    else
+      redirect to '/login'
+    end
   end
 
-  # GET: /projects/new
-  get "/projects/new" do
-    erb :"/projects/new.html"
+  get '/projects/new' do
+    if logged_in?
+      erb :'projects/new.html'
+    else
+      redirect to '/login'
+    end
   end
 
-  # POST: /projects
-  post "/projects" do
-    redirect "/projects"
+  post '/projects' do
+    if logged_in?
+      if params[:github] == "" || params[:description] == "" || params[:module] == "" || params[:title] == ""
+        redirect to "/projects/new"
+      else
+        
+        @project = Project.create(:title => params[:title], :module => params[:module], :github => params[:github], :description => params[:description], :user_id => current_user.id)
+        binding.pry
+        if @project.save
+          redirect to "/projects/#{@project.id}"
+        else
+          redirect to "/projects/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-  # GET: /projects/5
-  get "/projects/:id" do
-    erb :"/projects/show.html"
+  get '/projects/:id' do
+    if logged_in?
+      @project = Project.find_by_id(params[:id])
+      erb :'projects/show.html'
+    else
+      redirect to '/login'
+    end
   end
 
-  # GET: /projects/5/edit
-  get "/projects/:id/edit" do
-    erb :"/projects/edit.html"
+  get '/projects/:id/edit' do
+    if logged_in?
+      @project = Project.find_by_id(params[:id])
+      if @project && @project.user == current_user
+        erb :'projects/edit.html'
+      else
+        redirect to '/projects'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-  # PATCH: /projects/5
-  patch "/projects/:id" do
-    redirect "/projects/:id"
+  patch '/projects/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/projects/#{params[:id]}/edit"
+      else
+        @project = Project.find_by_id(params[:id])
+        if @project && @project.user == current_user
+          if @project.update(:title => params[:title], :module => params[:module], :github => params[:github], :description => params[:description])
+            redirect to "/projects/#{@project.id}"
+          else
+            redirect to "/projects/#{@project.id}/edit"
+          end
+        else
+          redirect to '/projects'
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-  # DELETE: /projects/5/delete
-  delete "/projects/:id/delete" do
-    redirect "/projects"
+  delete '/projects/:id/delete' do
+    if logged_in?
+      @project = Project.find_by_id(params[:id])
+      if @project && @project.user == current_user
+        @project.delete
+      end
+      redirect to '/project'
+    else
+      redirect to '/login'
+    end
   end
+
+  # # GET: /projects
+  # get "/projects" do
+  #   erb :"/projects/index.html"
+  # end
+
+  # # GET: /projects/new
+  # get "/projects/new" do
+  #   erb :"/projects/new.html"
+  # end
+
+  # # POST: /projects
+  # post "/projects" do
+  #   redirect "/projects"
+  # end
+
+  # # GET: /projects/5
+  # get "/projects/:id" do
+  #   erb :"/projects/show.html"
+  # end
+
+  # # GET: /projects/5/edit
+  # get "/projects/:id/edit" do
+  #   erb :"/projects/edit.html"
+  # end
+
+  # # PATCH: /projects/5
+  # patch "/projects/:id" do
+  #   redirect "/projects/:id"
+  # end
+
+  # # DELETE: /projects/5/delete
+  # delete "/projects/:id/delete" do
+  #   redirect "/projects"
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
   get '/signup' do
     if !logged_in?
       erb :'users/new.html', locals: {message: "Please sign up before you sign in"}
@@ -9,10 +14,9 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    if params[:firstname] == "" || params[:lastname] == "" || params[:username] == "" || params[:password] == ""
       redirect to '/signup'
     else
-      binding.pry
       @user = User.new(:firstname => params[:firstname], :lastname => params[:lastname], :username => params[:username], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
@@ -20,33 +24,31 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/'
+    else
+      redirect to '/'
+    end
   end
 end
