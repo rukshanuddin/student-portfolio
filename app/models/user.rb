70e94357f65459749322cf1fc5fe27f48bf37751
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-01-30 17:54:02 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :password
+  validates :password, presence: true
 
 end
