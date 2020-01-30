59f5d858eec3428ab4067bb8592f90e4472616f8
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-01-30 14:32:57 -0500

    working on spec.md, validat presence of user attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many  :projects
   has_secure_password
 
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :username, presence: true, uniqueness: true
 end
